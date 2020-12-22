77afb6e5e6798ca6258fe627115e5c7cf8a6392d
yangliuzhu <yangliuzhu@tydic.com>
2020-12-22 18:19:26 +0800

    Initial commit

---
+++ src/main/java/config/CONST.java
@@ -0,0 +1,15 @@
+package config;
+
+
+public class CONST {
+
+
+    public static final String BASE_URL="https://api.miaodiyun.com/20150822";
+
+    public static final String ACCOUNT_SID="b8533b5fbcdd445b8071f0e0463997d";
+
+    public static final String AUTH_TOKEN="952ece5f5c634f75919b7516690a7d07";
+
+    public static final String RESP_DATA_TYPE="json";
+
+}
